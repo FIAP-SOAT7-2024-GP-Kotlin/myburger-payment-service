8df45465628b7ed8dd5dcf40b0ab1afe9c69c148
Jose Rocha <jsantosrocha@gmail.com>
2024-11-26 20:24:00 -0300

    fix PaymentIT

--- src/test/kotlin/io/github/soat7/myburguercontrol/fixtures/PaymentFixtures.kt
+++ src/test/kotlin/io/github/soat7/myburguercontrol/fixtures/PaymentFixtures.kt
@@ -4,10 +4,28 @@ import io.github.soat7.myburguercontrol.domain.entities.Payment
 import io.github.soat7.myburguercontrol.domain.entities.enum.PaymentStatus
 import io.github.soat7.myburguercontrol.external.thirdparty.api.QRCodeData
 import io.github.soat7.myburguercontrol.external.webservice.payment.api.OrderItem
+import io.github.soat7.myburguercontrol.external.webservice.payment.api.PaymentCreationRequest
 import io.github.soat7.myburguercontrol.util.toBigDecimal
 import java.util.UUID
 
 object PaymentFixtures {
+    fun mockPaymentRequest(orderId: UUID): PaymentCreationRequest {
+        return PaymentCreationRequest(
+            orderId = orderId,
+            orderPrice = 10.0.toBigDecimal(),
+            orderItems = listOf(
+                OrderItem(
+                    title = "item1",
+                    description = "descricao1",
+                    unitPrice = 10.0.toBigDecimal(),
+                    quantity = 1,
+                    unitMeasure = "Unit",
+                    totalAmount = 10.0.toBigDecimal(),
+                ),
+            ),
+        )
+    }
+
     fun mockPayment(orderId: UUID): Payment {
         return Payment(
             id = UUID.randomUUID(),
--- src/test/kotlin/io/github/soat7/myburguercontrol/webservice/PaymentIT.kt
+++ src/test/kotlin/io/github/soat7/myburguercontrol/webservice/PaymentIT.kt
@@ -1,8 +1,15 @@
 package io.github.soat7.myburguercontrol.webservice
 
+import io.github.soat7.myburguercontrol.adapters.gateway.PaymentIntegrationRepository
 import io.github.soat7.myburguercontrol.base.BaseIntegrationTest
+import io.github.soat7.myburguercontrol.external.db.payment.PaymentGateway
 import io.github.soat7.myburguercontrol.external.webservice.payment.api.PaymentResponse
+import io.github.soat7.myburguercontrol.fixtures.HttpMockRequest
 import io.github.soat7.myburguercontrol.fixtures.PaymentFixtures
+import io.github.soat7.myburguercontrol.fixtures.PaymentFixtures.mockQRCode
+import io.mockk.clearMocks
+import io.mockk.mockk
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -13,31 +20,39 @@ import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import java.util.UUID
+import kotlin.test.BeforeTest
 
 class PaymentIT : BaseIntegrationTest() {
+
+    private val paymentIntegrationRepository = mockk<PaymentIntegrationRepository>()
+    private val paymentGateway = mockk<PaymentGateway>()
+
+    @BeforeTest
+    fun setUp() {
+        clearMocks(paymentIntegrationRepository)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        clearMocks(paymentIntegrationRepository, paymentGateway)
+    }
 
     @Test
     fun `should successfully create a new payment`() {
         val orderId = UUID.randomUUID()
-        val inputCustomerData = PaymentFixtures.mockPayment(orderId)
+        val paymentRequest = PaymentFixtures.mockPaymentRequest(orderId)
 
+        HttpMockRequest.mockQRCodeDataResponse(mockQRCode(orderId.toString()))
         val response = restTemplate.exchange<PaymentResponse>(
             url = "/payment",
             method = HttpMethod.POST,
-            requestEntity = HttpEntity(inputCustomerData),
+            requestEntity = HttpEntity(paymentRequest),
         )
 
         assertAll(
             Executable { assertTrue(response.statusCode.is2xxSuccessful) },
             Executable { assertNotNull(response.body) },
             Executable { assertEquals(orderId, response.body!!.orderId) },
-        )
-        val savedPayment = paymentJpaRepository.findById(UUID.fromString(response.body!!.id))
-
-        assertAll(
-            Executable { assertNotNull(savedPayment) },
-            Executable { assertEquals(orderId, savedPayment.get().orderId) },
-            Executable { assertEquals(response.body!!.id, savedPayment.get().id) },
         )
     }
 }
